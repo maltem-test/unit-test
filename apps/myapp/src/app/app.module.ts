2d119ddf8dbf61c2e9a3a91cd1b2a910d9f7a687
HB5850 <oussama.mejid@external.engie.com>
2019-05-21 16:44:16 +0200

    initial

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonneCreationValidatorService } from './validators/personne-creation-validator.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [PersonneCreationValidatorService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
