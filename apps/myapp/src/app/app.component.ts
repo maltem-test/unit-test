fb14adc4e3949f89913ee1a870b8c4b78f96b412
HB5850 <oussama.mejid@external.engie.com>
2019-05-21 17:51:36 +0200

    wip

--- apps/myapp/src/app/app.component.ts
+++ apps/myapp/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { PersonneCreationValidatorService } from './validators/personne-creation-validator.service';
 
 @Component({
   selector: 'jestworkspace-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [PersonneCreationValidatorService]
 })
+
 export class AppComponent {
   title = 'myapp';
+
+  constructor( private personneCreationValidatorService: PersonneCreationValidatorService) {}
+
+  
+  filterValidator(){
+
+  }
+
+  
 }
--- apps/myapp/src/app/validators/personne-creation-validator.service.ts
+++ apps/myapp/src/app/validators/personne-creation-validator.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class PersonneCreationValidatorService {
 
   constructor() { }
+
+  filterValidator(filter: object) {
+    console.log(filter);
+  }
 }
