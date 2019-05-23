93f21763c28e063c2efdecb782e53edd312fce4e
HB5850 <oussama.mejid@external.engie.com>
2019-05-23 09:59:47 +0200

    initial test

--- apps/myapp/src/app/validators/validator.service.ts
+++ apps/myapp/src/app/validators/validator.service.ts
@@ -1,40 +1,54 @@
 import { Injectable } from '@angular/core';
-
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 export interface ValidatedFields {
-    siteIdList: number[];
-    text: string;
+  siteIdList: number[];
+  text: string;
 }
 
 @Injectable()
 export class CommonStringMaxLengthValidatorGenService {
-    validateGen(length: number) {
-        return (str: string) => str.length > length;
-    }
+  validateGen(length: number) {
+    return (str: string) => str.length > length;
+  }
 }
 
 @Injectable()
 export class PersonneCreationFormValidatorService {
-    static errorCodeTextCompliantWithBackEnd = 100;
-    static errorCodeTextCompliantWithPos = 101;
+  static errorCodeTextCompliantWithBackEnd = 100;
+  static errorCodeTextCompliantWithPos = 101;
 
-    constructor(private commonStringMaxLengthValidatorGenService: CommonStringMaxLengthValidatorGenService) {}
+  constructor(
+    private commonStringMaxLengthValidatorGenService: CommonStringMaxLengthValidatorGenService
+  ) {}
 
-    validate({ siteIdList, text }) {
-        const textErrors = [];
-        const textErrorsIsNotCompliantWithBackEnd = this.commonStringMaxLengthValidatorGenService.validateGen(5000)(text);
-        if (textErrorsIsNotCompliantWithBackEnd) {
-            textErrors.push(PersonneCreationFormValidatorService.errorCodeTextCompliantWithBackEnd);
-        }
-        const textErrorsIsNotCompliantWithPos = this.commonStringMaxLengthValidatorGenService.validateGen(20)(text);
-        if (textErrorsIsNotCompliantWithPos) {
-            textErrors.push(PersonneCreationFormValidatorService.errorCodeTextCompliantWithPos);
-        }
-        return textErrors;
+  validate({ siteIdList, text }) {
+    const textErrors = [];
+    const minThan50CharValidator  =  this.commonStringMaxLengthValidatorGenService.validateGen(
+        5000
+      );
+    const textErrorsIsNotCompliantWithBackEnd = minThan50CharValidator(text);
+    if (textErrorsIsNotCompliantWithBackEnd) {
+      textErrors.push(
+        PersonneCreationFormValidatorService.errorCodeTextCompliantWithBackEnd
+      );
     }
-
-    getRandom(min, max) {
-        let random = null;
-        setTimeout(() => { random = Math.random()*(max-min);}, 2000);
-        return random;
+    const textErrorsIsNotCompliantWithPos = this.commonStringMaxLengthValidatorGenService.validateGen(
+      20
+    )(text);
+    if (textErrorsIsNotCompliantWithPos) {
+      textErrors.push(
+        PersonneCreationFormValidatorService.errorCodeTextCompliantWithPos
+      );
     }
+    return textErrors;
+  }
+
+  getRandom({ min, max }: { min: number; max: number }) {
+    return of(Math.random() * (max - min)).pipe(delay(500));
+  }
+
+  getFloor({ nmb }: { nmb: number }) {
+    return of(Math.floor(nmb)).pipe(delay(500));
+  }
 }
